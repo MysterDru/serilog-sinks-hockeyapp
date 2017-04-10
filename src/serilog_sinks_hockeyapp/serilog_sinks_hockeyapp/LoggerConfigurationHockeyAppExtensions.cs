dc69729e7b66ee5b4142e795401178d0fcff905f
Drew Frisk <hello@drewfrisk.com>
2017-04-09 20:19:43 -0500

    initial add of sink implementation

---
+++ src/serilog_sinks_hockeyapp/serilog_sinks_hockeyapp/LoggerConfigurationHockeyAppExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Serilog.Configuration;
+using Serilog.Events;
+using Serilog.Formatting.Display;
+
+namespace Serilog
+{
+	/// <summary>
+	/// Adds WriteTo.NSLog() to the logger configuration.
+	/// </summary>
+	public static class LoggerConfigurationHockeyAppExtensions
+	{
+		const string DefaultNSLogOutputTemplate = "[{Level}] {Message:l}{NewLine:l}{Exception:l}";
+
+		/// <summary>
+		/// Adds a sink that writes log events to HockeyApp events
+		/// </summary>
+		/// <param name="sinkConfiguration">The configuration being modified.</param>
+		/// <param name="restrictedToMinimumLevel">The minimum log event level required in order to write an event to the sink.</param>
+		/// <param name="outputTemplate">Template for the output events</param>
+		/// <param name="formatProvider">Supplies culture-specific formatting information, or null.</param>
+		/// <exception cref="ArgumentNullException">A required parameter is null.</exception>
+		public static LoggerConfiguration HockeyApp(this LoggerSinkConfiguration sinkConfiguration,
+			LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum,
+			string outputTemplate = DefaultNSLogOutputTemplate,
+			IFormatProvider formatProvider = null)
+		{
+
+			if (sinkConfiguration == null)
+				throw new ArgumentNullException("sinkConfiguration");
+
+			if (outputTemplate == null)
+				throw new ArgumentNullException("outputTemplate");
+
+			var formatter = new MessageTemplateTextFormatter(outputTemplate, formatProvider);
+			return sinkConfiguration.Sink(new Sinks.HockeyApp.HockeyAppSink(formatter), restrictedToMinimumLevel);
+		}
+	}
+
+}
---
+++ src/serilog_sinks_hockeyapp/serilog_sinks_hockeyapp/Sinks/HockeyApp/HockeyAppSink.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using HockeyApp;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Formatting;
+
+namespace Serilog.Sinks.HockeyApp
+{
+	public class HockeyAppSink : ILogEventSink
+	{
+		readonly ITextFormatter _textFormatter;
+
+		public HockeyAppSink(ITextFormatter formatter)
+		{
+			_textFormatter = formatter;
+		}
+
+		public void Emit(LogEvent logEvent)
+		{
+			if (logEvent == null) throw new ArgumentNullException(nameof(logEvent));
+
+			Dictionary<string, string> propertys = new Dictionary<string, string>();
+			foreach (var property in logEvent.Properties)
+			{
+				propertys.Add(property.Key, property.Value.ToString());
+			}
+			propertys.Add(nameof(logEvent.Timestamp), logEvent.Timestamp.ToString());
+
+			var renderSpace = new StringWriter();
+			_textFormatter.Format(logEvent, renderSpace);
+
+			MetricsManager.TrackEvent(renderSpace.ToString(), propertys, null);
+		}
+	}
+}
